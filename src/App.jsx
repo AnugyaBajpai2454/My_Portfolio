81ca03abc9ce75a7765253dd38ce851518f923ed
Anugya Bajpai <bajpaianugyaab@gmail.com>
2024-07-31 18:42:09 +0530

    Project Section Commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { About } from "./componets/About/About";
 import { Experience } from "./componets/Experience/Experience";
 import { Hero } from "./componets/Hero/Hero";
 import { Navbar } from "./componets/Navbar/Navbar";
+import { Projects } from "./componets/Projects/Projects";
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
     <Hero />
     <About/>
     <Experience />
+    <Projects />
   </div>
   );
 }
